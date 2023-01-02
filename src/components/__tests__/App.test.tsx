71be9ef1cdbda2315166c81f490697ea0cea1bd3
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2023-01-02 11:46:42 +0000

    tests: updates tests to current usage guidelines

--- src/components/__tests__/App.test.tsx
+++ src/components/__tests__/App.test.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { render, screen, fireEvent } from '@testing-library/react'
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from '../App';
 
 jest.mock('../../utils/config', () => ({
@@ -7,10 +7,10 @@ jest.mock('../../utils/config', () => ({
         return {
             width: 10,
             height: 10,
-            bugs: 2
-        }
-    }
-}))
+            bugs: 2,
+        };
+    },
+}));
 
 test('shows initial message', () => {
     const { asFragment } = render(<App />);
@@ -19,90 +19,91 @@ test('shows initial message', () => {
     expect(asFragment()).toMatchSnapshot();
 });
 
-test('right click tile adds flag icon', async () => {
-    const { getByTestId, getAllByTestId } = render(<App />);        
-    const eventNode = getAllByTestId('tile')[0];
+test('right click tile adds flag icon', () => {
+    render(<App />);
+
+    const eventNode = screen.getAllByTestId('tile')[0];
 
     fireEvent.contextMenu(eventNode);
 
-    const tile = await getByTestId('flag-icon');
+    const flagTile = screen.getByTestId('flag-icon');
 
-    expect(tile).toBeTruthy();    
+    expect(flagTile).toBeTruthy();
 });
 
-test('right click tile updates bug count', async () => {
-    const { getByText, getAllByTestId } = render(<App customBugIndexes={[0]} />);
-    const eventNode = getAllByTestId('tile')[0];
+test('right click tile updates bug count', () => {
+    render(<App customBugIndexes={[0]} />);
+    const eventNode = screen.getAllByTestId('tile')[0];
 
     fireEvent.contextMenu(eventNode);
 
-    const titleText = await getByText('Find 1 bugs');
+    const titleText = screen.getByText('Find 1 bugs');
 
     expect(titleText).toBeTruthy();
 });
 
-test('pressing on bug ends game', async () => {
-    const { getByText, getAllByTestId } = render(<App customBugIndexes={[0]} />);
-    const eventNode = getAllByTestId('tile')[0];
+test('pressing on bug ends game', () => {
+    render(<App customBugIndexes={[0]} />);
+    const eventNode = screen.getAllByTestId('tile')[0];
 
     fireEvent.click(eventNode);
 
-    const titleText = await getByText('Oops Game Over!');
+    const titleText = screen.getByText('Oops Game Over!');
 
     expect(titleText).toBeTruthy();
 });
 
-test('pressing on bug shows bug icon', async () => {
-    const { getByTestId, getAllByTestId } = render(<App customBugIndexes={[0]} />);
-    const eventNode = getAllByTestId('tile')[0];
+test('pressing on bug shows bug icon', () => {
+    render(<App customBugIndexes={[0]} />);
+    const eventNode = screen.getAllByTestId('tile')[0];
 
     fireEvent.click(eventNode);
 
-    const tile = await getByTestId('bug-icon');
+    const tile = screen.getByTestId('bug-icon');
 
-    expect(tile).toBeTruthy();  
+    expect(tile).toBeTruthy();
 });
 
-test('pressing on non bug clears tiles', async () => {
-    const { getAllByTestId } = render(<App customBugIndexes={[0]} />);
-    const eventNode = getAllByTestId('tile')[5];
+test('pressing on non bug clears tiles', () => {
+    render(<App customBugIndexes={[0]} />);
+    const eventNode = screen.getAllByTestId('tile')[5];
 
     fireEvent.click(eventNode);
 
-    const tile = await getAllByTestId('tile-showing');
+    const tile = screen.getAllByTestId('tile-showing');
 
-    expect(tile).toBeTruthy(); 
+    expect(tile).toBeTruthy();
 });
 
-test('pressing new game resets board', async () => {
-    const { getByText, getAllByTestId } = render(<App customBugIndexes={[0]} />);
-    const eventNode = getAllByTestId('tile')[5];
+test('pressing new game resets board', () => {
+    render(<App customBugIndexes={[0]} />);
+    const eventNode = screen.getAllByTestId('tile')[5];
 
     fireEvent.click(eventNode);
 
-    await getAllByTestId('tile-showing');
+    screen.getAllByTestId('tile-showing');
 
-    const button = getByText('New Game');
+    const button = screen.getByText('New Game');
 
     fireEvent.click(button);
 
-    const tiles = await getAllByTestId('tile');
+    const tiles = screen.getAllByTestId('tile');
 
-    expect(getByText('Find 2 bugs')).toBeTruthy();
-    expect(tiles).toHaveLength(100); 
+    expect(screen.getByText('Find 2 bugs')).toBeTruthy();
+    expect(tiles).toHaveLength(100);
 });
 
-test('on flagging all bugs win state is shown', async () => {
-    const { getByText, getAllByTestId } = render(<App customBugIndexes={[0, 1]} />);
-    const bug1 = getAllByTestId('tile')[0];
-    const bug2 = getAllByTestId('tile')[1];
-    const nonBug = getAllByTestId('tile')[99];
+test('on flagging all bugs win state is shown', () => {
+    render(<App customBugIndexes={[0, 1]} />);
+    const bug1 = screen.getAllByTestId('tile')[0];
+    const bug2 = screen.getAllByTestId('tile')[1];
+    const nonBug = screen.getAllByTestId('tile')[99];
 
     fireEvent.contextMenu(bug1);
     fireEvent.contextMenu(bug2);
     fireEvent.click(nonBug);
 
-    const headerText = await getByText('You Win!');
+    const headerText = screen.getByText('You Win!');
 
     expect(headerText).toBeTruthy();
 });
